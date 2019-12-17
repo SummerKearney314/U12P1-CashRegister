c317442ef5b6bc252513c0ddc8c633a0f7fd0c27
Summer Kearney <summer.kearney314@sccdsb.net>
2019-12-17 11:51:38 -0500

    filling in the rest of the methods

--- CashRegister.java
+++ CashRegister.java
@@ -12,6 +12,10 @@ public class CashRegister
 {
     public static void main (String[] args) throws IOException
     {
+
+	BufferedReader kbInp = new BufferedReader (new InputStreamReader (System.in));
+
+
 	// Read in the prices
 	double[] prices = readPriceList ();
 
@@ -52,28 +56,138 @@ public class CashRegister
     // These variable can be accessed in any method.  Your are encouraged to
     // make use of them.
     final static int[] DENOMINATION_CENTS = {
-	10000, 5000, 2000, 1000, 500, 200, 100, 25, 10, 5
-	};
+	10000, 5000, 2000, 1000, 500, 200, 100, 25, 10, 5};
     final static String[] DENOMINATION_NAMES = {
 	"$100 Bills", "$50 Bills", "$20 Bills", "$10 Bills", "$5 Bills",
-	"Toonies", "Loonies", "Quarters", "Dimes", "Nickels"
-	};
+	"Toonies", "Loonies", "Quarters", "Dimes", "Nickels"};
 
 
 
-    // TODO: IMPLEMENT YOUR METHODS HERE
-    
     /**
 	*Reads in the list of prices. It then returns a double
 	    array in the size of the list, in order to read in
 	    the list of prices
     */
-    public static void readPriceList ()
+    public static double[] readPriceList () throws IOException
     {
-    BufferedReader kbInp = new BufferedReader (new InputStreamReader (System.in));
+	BufferedReader kbInp = new BufferedReader (new InputStreamReader (System.in));
 
-	System.err.println ("How many prices are there?");
-	int size = (kbInp.readLine ());
+	// Read in a character
+	System.err.print ("Enter the number of prices: ");
+	int listSize = Integer.parseInt (kbInp.readLine ());
+
+	System.err.println ("Enter the prices:");
+
+	double[] pricesArray = new double [listSize];
+
+	for (int index1 = 0 ; index1 < listSize ; index1++)
+	{
+	    pricesArray [index1] = Double.parseDouble (kbInp.readLine ());
+	}
+	return (pricesArray);
+    }
+
+
+    /**
+	    * Adds up all of the total prices previusly entered by the user.
+	     Then returns the total as a double
+	*/
+    public static double sum (double[] prices)
+    {
+	double sum = 0;
+	for (int index1 = 0 ; index1 < prices.length  ; index1++)
+	{
+	    sum = sum + prices [index1];
+	}
+	return sum;
+    }
+
+
+    /**
+	* Calculates the tax of the subtotal by multiplying the subtotal
+	  by 0.13. Then returns it as a double
+    */
+    public static double calculateTax (double subtotal, double SALES_TAX_RATE)
+    {
+	return (subtotal * SALES_TAX_RATE);
+
+    }
+
+    /**
+	*Rounds the amount up to the nearest fith cent, as in Canada
+	the penny is no longer made, making the amount round
+    */
+    public static double roundToNickel (double total)
+    {
+	// Splits space separated list.
+	//System.out.println (Math.round(total *100)/100.0);
+	double cashRounded = Math.round (total * 20) / 20.0;
+	return cashRounded;
+
+    }
+
+
+
+
+   /**
+	    *Asks the user how much cash was paid. Then returned.
+    */
+    public static double readCashAmount () throws IOException
+    {
+	BufferedReader kbInp = new BufferedReader (new InputStreamReader (System.in));
+	System.err.print ("How much cash was paid? ");
+	return (Integer.parseInt (kbInp.readLine ()));
+    }
+
+
+    /**
+	*akes in two amounts: the total cost of the
+	items and the amount paid. The method returns 
+	an array of 10 integers representing how many 
+	of each money denomination is needed for change.  
+    */
+    public static int[] makeChange (double totalCost, double cashPaid)
+    {
 	
+	int centsPaid = (int) cashPaid * 100;
+    
+	double centsTotal =  totalCost * 100;
+	int change = (int) centsPaid - (int) centsTotal;
+
+	    
+	int[] changeArray = new int [10];
+
+	for (int index = 0 ; index < DENOMINATION_CENTS.length ; index++)
+	{
+	    
+	    changeArray [index] = change / DENOMINATION_CENTS [index];
+
+
+	    change = change - (changeArray [index] * DENOMINATION_CENTS [index]);
+
+	}
+
+	return (changeArray);
+
+
+    }
+
+
+    /**
+	    *takes in an array of 10 integers as is 
+	    output from the above method and prints the change as in the example below. 
+	    Only denominations that are needed should be printed.  No value is returned.
+	*/
+    public static void printChange (int [] change)
+    {
+	for (int index = 0 ; index < DENOMINATION_CENTS.length ; index++)
+	{
+	    if (change [index] != 0)
+	    {
+	    System.out.println (" - " + DENOMINATION_NAMES [index] + ":" + change [index]);
+	    }
+	}
     }
 }
+
+
